a073ca6f11a620ef70c55e41e2997d0a9e0ff7e3
Camden Hess <camden.hess12@gmail.com>
2024-02-07 16:01:20 -0700

    added complete functionality to wheel

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,9 +1,25 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE } from '../state/action-types'
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
-  return state
+  let num = state;
+  switch(action.type) {
+    
+    case MOVE_CLOCKWISE:
+      if (state === 5) num = 0
+       else num++
+      return num
+
+    case MOVE_COUNTERCLOCKWISE:
+      if (state === 0) num = 5
+        else num--
+      return num
+
+    default:
+      return state
+  }
 }
 
 const initialQuizState = null
